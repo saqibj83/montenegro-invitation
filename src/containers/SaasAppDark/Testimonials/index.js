e4e2d1cc0dff12d9d2482a0d08751ca244c6fed1
Muhammad Saqib <saqib2uk@hotmail.com>
2022-08-15 07:50:05 +0500

    pl-28

--- src/containers/SaasAppDark/Testimonials/index.js
+++ src/containers/SaasAppDark/Testimonials/index.js
@@ -29,6 +29,7 @@ import image24 from "common/assets/image/player24.jpg";
 import image25 from "common/assets/image/player25.jpg";
 import image26 from "common/assets/image/player26.jpg";
 import image27 from "common/assets/image/player27.jpg";
+import image28 from "common/assets/image/player28.jpg";
 import image29 from "common/assets/image/player29.jpg";
 import image30 from "common/assets/image/player30.jpg";
 function NextArrow(props) {
@@ -631,7 +632,7 @@ const Testimonials = () => {
                   />
                   <NextImage
                     className="cyprus-img"
-                    src={imageDef}
+                    src={image28}
                     alt="Mustapha Kanit"
                   />
                 </div>
