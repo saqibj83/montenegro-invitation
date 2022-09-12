ac8bbab2a021d7254675e3126aaa7c559f022107
Muhammad Saqib <saqib2uk@hotmail.com>
2022-09-12 17:24:46 +0500

    ply plus -live streamm

--- src/containers/SaasClassic/Banner/index.js
+++ src/containers/SaasClassic/Banner/index.js
@@ -69,7 +69,7 @@ const BannerSection = ({ row, contentWrapper, title, imageWrapper }) => {
           <Box>
             <iframe
               className="ifram-wd"
-              src="https://www.youtube.com/embed/a-pc3_J0eKM"
+              src="https://www.youtube.com/embed/IbqMsxQh6C8"
               title="YouTube video player"
               frameborder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
