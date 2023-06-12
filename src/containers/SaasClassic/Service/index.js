64c849d699e1f8906e385767377c23e65f172836
saqibj83 <98015804+saqibj83@users.noreply.github.com>
2023-06-12 08:01:07 +0500

    Update index.js

--- src/containers/SaasClassic/Service/index.js
+++ src/containers/SaasClassic/Service/index.js
@@ -21,11 +21,6 @@ const ServiceSection = ({ secTitleWrapper, secHeading }) => {
             content="  £200,000 NLH
             "
           />
-//           <Heading
-//             {...secHeading}
-//             content="   
-//             "
-//           />
               <h2>3 <sup>rd</sup> - 5<sup>th</sup> Aug 2023 </h2>
           <p>
             The Luxon Invitational is a unique £200,000 NLH buy-in that features
