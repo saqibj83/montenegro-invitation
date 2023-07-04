bf2120c4a7443a5c5a9f0dbd409409de92d94e1e
Muhammad Saqib <saqib2uk@hotmail.com>
2023-07-04 15:05:48 +0500

    pair10

--- src/containers/SaasAppDark/Testimonials/index.js
+++ src/containers/SaasAppDark/Testimonials/index.js
@@ -1057,33 +1057,35 @@ const Testimonials = () => {
             <div className="grid-x">
               <div className=" cell small main_card__invites">VIP</div>
               <div className="cell small gold-card-header">
-                {/* <img
+                <img
                   className="flag-PlayerPhoto"
-                  src="https://prod-assets.tritonpoker.plus/flags/1x1/my.svg"
+                  src="https://prod-assets.tritonpoker.plus/flags/1x1/id.svg"
                   alt="player"
-                /> */}
-                <div className="main_card__image pd-6">
+                />
+                <div className="main_card__image ">
                   <NextImage
                     className="cyprus-img"
-                    src={imageDef}
-                    alt="Paul Phua"
+                    src={image19}
+                    alt="FERDINAND PUTRA"
                   />
                 </div>
-                {/* <h4 className="main_card__name">Paul Phua</h4> */}
+                <h4 className="main_card__name">FERDINAND PUTRA</h4>
               </div>
+
               <div className=" cell small main_card__invites">INVITES</div>
+
               <div className="cell small main__card__bottom">
-                {/* <h4 className="main_card__name"> Rui Cao </h4> */}
-                <div className="main_card__image pd-6">
-                  {/* <img
+                <h4 className="main_card__name">TIMOTHY ADAMS</h4>
+                <div className="main_card__image">
+                  <img
                     className="flag-PlayerPhoto"
-                    src="https://prod-assets.tritonpoker.plus/flags/1x1/fr.svg"
+                    src="https://prod-assets.tritonpoker.plus/flags/1x1/ca.svg"
                     alt="player"
-                  /> */}
+                  />
                   <NextImage
                     className="cyprus-img"
-                    src={imageDef}
-                    alt="Rui Cao"
+                    src={image20}
+                    alt="TIMOTHY ADAMS  "
                   />
                 </div>
               </div>
