bd94f62dd0ad629b3f86827d060bf80d0766e036
Muhammad Saqib <saqib2uk@hotmail.com>
2023-10-20 10:07:40 +0500

    3rd aair

--- src/containers/SaasAppDark/Testimonials/index.js
+++ src/containers/SaasAppDark/Testimonials/index.js
@@ -1213,7 +1213,7 @@ const Testimonials = () => {
             </div>
           </div>
 
-          {/* <div className="main_card cell auto">
+          <div className="main_card cell auto">
             <div className="grid-x">
               <div className=" cell small main_card__invites">INVITEE</div>
               <div className="cell small gold-card-header">
@@ -1251,7 +1251,7 @@ const Testimonials = () => {
               </div>
               <div className=" cell small main_card__invites">PRO PLAYER</div>
             </div>
-          </div> */}
+          </div>
           {/*
           <div className="main_card cell auto">
             <div className="grid-x">
