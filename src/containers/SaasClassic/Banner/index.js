80e4a381f5c906111e514485e983db0facbb2ccd
saqibj83 <98015804+saqibj83@users.noreply.github.com>
2024-08-07 22:11:01 +0800

    fod

--- src/containers/SaasClassic/Banner/index.js
+++ src/containers/SaasClassic/Banner/index.js
@@ -79,7 +79,7 @@ const BannerSection = ({ row, contentWrapper, title, imageWrapper }) => {
 
             <iframe
               className="ifram-wd"
-              src="https://www.youtube.com/embed/HJxTVoJFJ8I"
+              src="https://www.youtube.com/embed/XLVJ9bnHgIA"
               title="YouTube video player"
               frameborder="0"
               allow="accelerometer; controls; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
